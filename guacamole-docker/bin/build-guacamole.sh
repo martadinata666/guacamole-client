b96593ef21214fa9c9fd64b8de178a6ddfaa7ddb
Dedy Martadinata S <dedyms@pm.me>
2022-04-28 18:17:01 +0700

    Enable multiarch

--- guacamole-docker/bin/build-guacamole.sh
+++ guacamole-docker/bin/build-guacamole.sh
@@ -66,9 +66,9 @@ cd "$BUILD_DIR"
 export OPENSSL_CONF=/etc/ssl
 
 if [ -z "$BUILD_PROFILE" ]; then
-    mvn package
+    mvn -Drat.numUnapprovedLicenses=9999 package
 else
-    mvn -P "$BUILD_PROFILE" package
+    mvn -Drat.numUnapprovedLicenses=9999 -P "$BUILD_PROFILE" package
 fi
 
 #
